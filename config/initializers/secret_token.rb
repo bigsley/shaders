85e2b519ea6ea34850cb32a321f53e5875e9992f
bigsley <bigsley@gmail.com>
2012-10-29 11:40:18 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shaders::Application.config.secret_token = 'dd6d3424114ca785ecaa7b8127e2d2304b393fcfcf18d91bb273aebd6c4998a98f58a134adda40d93c7f7011e47ca7423881665146329fe77eb587b17b4f8dab'
